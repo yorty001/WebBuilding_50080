a4b4d8f1fbee3b35c5603d4ad0b7e37ba7e2f4b0
Taye Chase <chaty141@mymail.unisa.edu.au>
2024-09-05 16:20:45 +0930

    Starting the payment server will need to test that it is all functioning when its finished.

---
+++ WebBuilding_50080/Controllers/paymentService.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebBuilding_50080.Services
+{
+    public class PaymentService 
+    {
+        public bool ProcessPayment (int customerID, decimal amount)
+        {
+
+            return true;
+        }
+        
+    }
+}
