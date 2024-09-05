c580ae17186ebf20c10445e32ebd8edb6b724a3e
yorty001 <yorty001@mymail.unisa.edu.au>
2024-09-05 13:03:47 +0930

    Add project files.

---
+++ WebBuilding_50080/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebBuilding_50080.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
