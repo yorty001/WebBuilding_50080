f9f833e626c144fda53bafbd1515d76833f70682
Taye Chase <chaty141@mymail.unisa.edu.au>
2024-09-05 16:31:55 +0930

    Registering PaymentService.cs in program.cs

--- WebBuilding_50080/Program.cs
+++ WebBuilding_50080/Program.cs
@@ -1,10 +1,13 @@
+using WebBuilding_50080.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
-
+builder.Services.AddScoped<PaymentService>();
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
