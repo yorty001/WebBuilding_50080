b028992bc950ea88ec4247ab30a357a649bf064b
Ethan Nelson Fernandes <ferey011@mymail.unisa.edu.au>
2024-09-05 16:27:42 +0930

    Made changes to bg color and controller

--- WebBuilding_50080/Controllers/PetrolStationsController.cs
+++ WebBuilding_50080/Controllers/PetrolStationsController.cs
@@ -11,8 +11,21 @@ namespace WebBuilding_50080.Controllers
 
         public IActionResult UpdateFuelPrice()
         {
-            return View();
+            var fuelPrices = new List<FuelPriceModel>
+            {
+                new FuelPriceModel { FuelType = "Diesel", CurrentPrice = 1.50 },
+                new FuelPriceModel { FuelType = "Unleaded", CurrentPrice = 1.35 },
+                new FuelPriceModel { FuelType = "Premium", CurrentPrice = 1.70 }
+            };
+
+            return View(fuelPrices);
         }
 
+    }
+    public class FuelPriceModel
+    {
+        public string FuelType { get; set; }
+        public double CurrentPrice { get; set; }
+        public string UpdatedPrice { get; set; }
     }
 }
