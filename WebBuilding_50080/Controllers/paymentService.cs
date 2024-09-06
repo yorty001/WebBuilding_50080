48ad8ce4719a6108734c1ae73beb2ccf45409781
Taye Chase <chaty141@mymail.unisa.edu.au>
2024-09-06 13:22:24 +0930

    Fixing the payment serivce.cs had some errors still needs some changes.

--- WebBuilding_50080/Controllers/paymentService.cs
+++ WebBuilding_50080/Controllers/paymentService.cs
@@ -1,14 +1,43 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Stripe;
+using Stripe.Checkout;
+using System.Collections.Generic;
 
 namespace WebBuilding_50080.Services
 {
-    public class PaymentService 
+    public class PaymentService
     {
-        public bool ProcessPayment (int customerID, decimal amount)
+        public PaymentService()
         {
-
+            StripeConfiguration.ApiKey = "sk_test_51PuVHZJPpXkyeWaBITMqQtb6z84fOaZvSQE50AEivL23rfCuXa2HBet2i2f6zK4mv4yKIKfimG0MFR5YF3J2JlrA00AJ3n1yPp";
+        }
+        public bool ProcessPayment(int customerID, decimal amount)
+        {
             return true;
         }
-        
+
+        public string CheckoutSession(string priceID)
+        {
+            var domain = "http://localhost:4242";
+            var options = new SessionCreateOptions
+            {
+                LineItems = new List<SessionLineItemOptions>
+                {
+                    new SessionLineItemOptions
+                    {
+                       // Price = priceID, 
+                       // Quantity = 1,    
+                    },
+                },
+                Mode = "payment",    
+                SuccessUrl = domain + "/accepted.html",   
+                CancelUrl = domain + "/declined.html",    
+            };
+
+            var service = new SessionService();
+            Session session = service.Create(options);
+
+         
+            return session.Url;
+        }
     }
 }
