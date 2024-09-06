18b85fff9ef740e5f34f27f54d8134d32e1640d5
Ridwan Ahmed <ahmry023@mymail.unisa.edu.au>
2024-09-06 17:46:32 +0930

    Added View Order

---
+++ WebBuilding_50080/Views/View Order/View.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebBuilding_50080.Views.View_Order
+{
+    public class ViewModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
