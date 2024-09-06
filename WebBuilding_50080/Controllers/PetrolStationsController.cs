7da496de57f411a7246338134fcf2dbb4a9c31cc
Ethan Nelson Fernandes <ferey011@mymail.unisa.edu.au>
2024-09-06 17:13:59 +0930

    made changes to the layout

--- WebBuilding_50080/Controllers/PetrolStationsController.cs
+++ WebBuilding_50080/Controllers/PetrolStationsController.cs
@@ -5,7 +5,7 @@ namespace WebBuilding_50080.Controllers
 {
     public class PetrolStationsController : Controller
     {
-        // This is a Static list to hold fuel prices (simulating a database)
+        
         private static List<FuelPriceModel> fuelPrices = new List<FuelPriceModel>
         {
             new FuelPriceModel { FuelType = "Diesel", CurrentPrice = 1.50 },
@@ -13,35 +13,35 @@ namespace WebBuilding_50080.Controllers
             new FuelPriceModel { FuelType = "Premium", CurrentPrice = 1.70 }
         };
 
-        // GET: Show current fuel prices for update
+        
         [HttpGet]
         public IActionResult UpdateFuelPrice()
         {
             return View(fuelPrices);
         }
 
-        // POST: This will Update the fuel prices
+        
         [HttpPost]
         public IActionResult UpdateFuelPrice(List<FuelPriceModel> updatedFuelPrices)
         {
-            // This will Update the current prices with the new prices
+            
             for (int i = 0; i < updatedFuelPrices.Count; i++)
             {
                 fuelPrices[i].CurrentPrice = updatedFuelPrices[i].UpdatedPrice;
             }
 
-            // This will Redirect to the Index page showing the updated prices
+            
             return RedirectToAction("Index");
         }
 
-        // GET: This will Display the updated fuel prices on the Index page
+        
         public IActionResult Index()
         {
-            return View(fuelPrices); // This will Pass the updated prices to the view
+            return View(fuelPrices); 
         }
     }
 
-    // This is a Simple model for the Fuel Prices
+    
     public class FuelPriceModel
     {
         public string FuelType { get; set; }
