4639e0a4ad766cc25ca9733b80cb5a85bda42e3d
Taye Chase <chaty141@mymail.unisa.edu.au>
2024-09-06 15:53:42 +0930

    Updating the servers and trying to make them more specific to cart payments instead of just one item at a time.

--- WebBuilding_50080/Program.cs
+++ WebBuilding_50080/Program.cs
@@ -20,6 +20,8 @@ app.UseRouting();
 
 app.UseAuthorization();
 
+app.UseEndpoints(endpoints => endpoints.MapControllers());
+
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
