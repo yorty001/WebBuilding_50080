9a9fde5c5183ae7787f41bea601d4f87f92d07bc
cosnm002@mymail.unisa.edu.au <cosnm002@mymail.unisa.edu.au>
2024-09-06 14:12:16 +0930

    Authenticating the login

--- WebBuilding_50080/Controllers/LoginController.cs
+++ WebBuilding_50080/Controllers/LoginController.cs
@@ -1,16 +1,54 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-
+using System.Data.SqlClient;
 
 namespace WebBuilding_50080.Controllers
 {
     public class LoginController : Controller
     {
-      
+        [HttpGet]
         public IActionResult Index()
         {
             return View();
         }
 
+        [HttpPost]
+        public IActionResult Login(string email, string pass)
+        {
+            string connectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\costa\\OneDrive\\Documents\\01 - University\\WebBuilding_50080\\WebBuilding_50080\\App_Data\\UTRDB.mdf;Integrated Security=True;Connect Timeout=30";
 
+            using (SqlConnection db = new SqlConnection(connectionString))
+            {
+                db.Open();
+                SqlCommand cmdQ = new SqlCommand(@"
+                    SELECT email, pass FROM Manager WHERE email = @Email AND pass = @Password", db);
+
+                cmdQ.Parameters.AddWithValue("@Email", email);
+                cmdQ.Parameters.AddWithValue("@Password", pass);
+
+                SqlDataReader reader = cmdQ.ExecuteReader();
+                if (reader.HasRows)
+                {
+                    // Redirect to a success page or handle successful login
+                    return RedirectToAction("Index","Home");
+                }
+                else
+                {
+                    // Handle login failure (e.g., display an error message)
+                    ViewData["LoginError"] = "Invalid email or password.";
+                    return View("Index");
+                }
+                
+            }
+        }
+
+        public IActionResult Success()
+        {
+            // A page to show after a successful login
+            return View();
+        }
     }
 }
+            
+        
+    
+
