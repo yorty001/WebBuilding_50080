1ce117f6bf560332ce3e82e5e0b46e86eee22e49
cosnm002@mymail.unisa.edu.au <cosnm002@mymail.unisa.edu.au>
2024-09-06 12:11:55 +0930

    Making login page

---
+++ WebBuilding_50080/Controllers/LoginController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+
+namespace WebBuilding_50080.Controllers
+{
+    public class LoginController : Controller
+    {
+      
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+    }
+}
