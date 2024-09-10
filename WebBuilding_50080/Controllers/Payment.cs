7df5a57896f5f313b943f0b40df1c169a835c9c3
Taye Chase <chaty141@mymail.unisa.edu.au>
2024-09-10 16:58:20 +0930

    Removing Code that is no longer needed

--- WebBuilding_50080/Controllers/Payment.cs
+++ WebBuilding_50080/Controllers/Payment.cs
@@ -8,22 +8,6 @@ namespace server.Controllers
     {
         public class Payment : Controller
         {
-            private readonly PaymentService _paymentService;
-
-            public Payment(PaymentService paymentService)
-            {
-                _paymentService = paymentService;
-            }
-
-            [HttpPost]
-            [Route("create-checkout-session")]
-            public IActionResult CreateCheckoutSession([FromBody] List<Cartitem> cartItems)
-            {
-                string sessionUrl = _paymentService.CheckoutSession(cartItems);
-
-                Response.Headers.Add("location", sessionUrl);
-                return new StatusCodeResult(303);
-            }
             public IActionResult Index()
             {
                 return View();
