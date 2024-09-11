2ca480b08d3e109b8cd0054c69357bc0479fb723
cosnm002@mymail.unisa.edu.au <cosnm002@mymail.unisa.edu.au>
2024-09-11 10:57:22 +0930

    one connection string

--- WebBuilding_50080/Controllers/PetrolStationsController.cs
+++ WebBuilding_50080/Controllers/PetrolStationsController.cs
@@ -1,12 +1,19 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.Web.CodeGenerators.Mvc.Templates.BlazorIdentity.Pages.Manage;
 using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
+using WebBuilding_50080.Models;
 
 namespace WebBuilding_50080.Controllers
 {
     public class PetrolStationsController : Controller
     {
-        
+        private readonly SqlConnection _db;
+        public PetrolStationsController(SqlConnection db)
+        {
+            _db = db;
+        }
         private static List<FuelPriceModel> fuelPrices = new List<FuelPriceModel>
         {
             new FuelPriceModel { FuelType = "Diesel", CurrentPrice = 1.50 },
@@ -18,7 +25,28 @@ namespace WebBuilding_50080.Controllers
         [HttpGet]
         public IActionResult UpdateFuelPrice()
         {
-            return View(fuelPrices);
+            _db.Open();
+            SqlCommand cmdQ = new SqlCommand("SELECT fuelType, pricePL FROM Fuel", _db);
+
+
+
+            SqlDataReader reader = cmdQ.ExecuteReader();
+            List<Fuel> fuelList = new List<Fuel>();
+
+            while (reader.Read())
+            {
+                Fuel fuel = new Fuel
+                {
+                    fuelType = reader["fuelType"].ToString(),
+                    pricePL = reader["pricePL"].ToString()
+                };
+
+                fuelList.Add(fuel);
+            }
+
+            _db.Close();
+            return View(fuelList);
+
         }
 
         
