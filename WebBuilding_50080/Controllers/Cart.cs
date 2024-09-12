dd86e3392770d18bef4e7317f168ac3d40de64ee
Ethan Nelson Fernandes <ferey011@mymail.unisa.edu.au>
2024-09-12 20:35:20 +0930

    SCRUM 17 : Added a Cart Button and Controller

---
+++ WebBuilding_50080/Controllers/Cart.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebBuilding_50080.Controllers
+{
+    public class Cart : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
