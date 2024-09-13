df745137a08ed4a6c83b2552dc5c2fe5f18cfe0a
Ethan Nelson Fernandes <ferey011@mymail.unisa.edu.au>
2024-09-13 15:09:43 +0930

    SCRUM 8 & 17: Finally got the Cart to work

--- WebBuilding_50080/Controllers/Cart.cs
+++ WebBuilding_50080/Controllers/Cart.cs
@@ -1,30 +1,43 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
+using WebBuilding_50080.Models;
 
 namespace WebBuilding_50080.Controllers
 {
     public class CartController : Controller
     {
-        public IActionResult Index(string productName, string currentCart)
+        private static List<Product> cartProducts = new List<Product>();
+
+        public IActionResult Index(string productName, decimal productPrice)
         {
-            var cart = new List<string>();
-
-            if (!string.IsNullOrEmpty(currentCart))
-            {
-                cart = new List<string>(currentCart.Split(','));
-            }
-
             if (!string.IsNullOrEmpty(productName))
             {
-                cart.Add(productName);
+                var product = new Product
+                {
+                    Name = productName,
+                    Price = productPrice 
+                };
+
+                cartProducts.Add(product);
             }
 
-            var updatedCart = string.Join(",", cart);
+            var cartViewModel = new CartViewModel
+            {
+                Products = cartProducts
+            };
 
-            ViewBag.CartItems = cart;
-            ViewBag.UpdatedCart = updatedCart;  
+            return View(cartViewModel);
+        }
 
-            return View();
+        public IActionResult Remove(string productName)
+        {
+            var productToRemove = cartProducts.Find(p => p.Name == productName);
+            if (productToRemove != null)
+            {
+                cartProducts.Remove(productToRemove);
+            }
+
+            return RedirectToAction("Index");
         }
     }
 }
---
+++ WebBuilding_50080/Models/ViewCartController.cs
@@ -0,0 +1,9 @@
+﻿namespace WebBuilding_50080.Models
+{
+    public class CartViewModel
+    {
+        public List<Product> Products { get; set; } = new List<Product>();
+
+        public decimal TotalPrice => Products.Sum(p => p.Price);
+    }
+}
