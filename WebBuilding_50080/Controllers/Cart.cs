4e7c8063af8070c33f6771490a084a1654960dac
Ethan Nelson Fernandes <ferey011@mymail.unisa.edu.au>
2024-10-01 17:21:31 +0930

    Updated the Cart controller and added an empty staff page

--- WebBuilding_50080/Controllers/Cart.cs
+++ WebBuilding_50080/Controllers/Cart.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 using System.Collections.Generic;
 using WebBuilding_50080.Models;
 
@@ -7,6 +8,7 @@ namespace WebBuilding_50080.Controllers
     public class CartController : Controller
     {
         private static List<Product> cartProducts = new List<Product>();
+        private static List<Order> Orders = new List<Order>();
 
         public IActionResult Index(string productName, decimal productPrice)
         {
@@ -39,5 +41,40 @@ namespace WebBuilding_50080.Controllers
 
             return RedirectToAction("Index");
         }
+
+        public IActionResult PlaceOrder()
+        {
+            var order = new Order
+            {
+                OrderId = Orders.Count + 1,
+                Products new List<Product>(cartProducts),
+                TotalPrice = CalculateTotalPrice(cartProducts),
+                IsReady = false
+            };
+
+            Orders.Add(order);
+            cartProducts.Clear();
+
+            return RedirectToAction("OrderConfirmation", new { orderId = order.OrderId });
+        }
+
+        public IActionResult OrderConfirmation(int orderId)
+        {
+            var order = Orders.Find(o => o.OrderId == orderId);
+
+            return View(order);
+        }
+
+        private decimal CalculateTotalPrice(List<Product> products)
+        {
+            decimal totalPrice = 0;
+            foreach (var product in products)
+            {
+                totalPrice += product.Price;
+            }
+            return totalPrice;
+        }
     }
 }
+
+
---
+++ WebBuilding_50080/Controllers/OrderManagementController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebBuilding_50080.Models;
+
+namespace WebBuilding_50080.Controllers
+{
+    public class OrderManagementController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View(CartController.Orders);
+        }
+
+        public IActionResult MarkAsReady(int orderId)
+        {
+            var order 
+        }
+    }
+}
