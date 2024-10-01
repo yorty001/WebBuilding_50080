e698bb99690db0ae71e4fc6b0539416e3b9dd5d9
Ethan Nelson Fernandes <ferey011@mymail.unisa.edu.au>
2024-10-01 17:49:19 +0930

    Added a Model file for the Order Status

---
+++ WebBuilding_50080/Models/OrderStatus.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebBuilding_50080.Models
+{
+    public class OrderStatus : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
