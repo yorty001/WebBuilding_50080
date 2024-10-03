4d597e521ddeae801c101e2f0b6f8f80b2c93787
Taye Chase <chaty141@mymail.unisa.edu.au>
2024-10-03 14:30:12 +0930

    Merge

--- WebBuilding_50080/Models/CartViewModel.cs
+++ WebBuilding_50080/Models/CartViewModel.cs
@@ -2,24 +2,8 @@
 {
     public class CartViewModel
     {
-        // Existing properties
         public List<Product> Products { get; set; } = new List<Product>();
 
         public decimal TotalPrice => Products.Sum(p => p.Price);
-
-        // New properties for payment details
-        public string? FullName { get; set; }
-        public string Email { get; set; }
-        public string Address { get; set; }
-        public string City { get; set; }
-        public string State { get; set; }
-        public string Zip { get; set; }
-
-        // Payment details
-        public string CardName { get; set; }
-        public string CardNumber { get; set; }
-        public string ExpMonth { get; set; }
-        public string ExpYear { get; set; }
-        public string CVV { get; set; }
     }
 }
