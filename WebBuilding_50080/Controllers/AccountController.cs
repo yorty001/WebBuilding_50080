5351f7e82a03ffddca6cfa669893df6ffeb24f26
Ridwan Ahmed <ahmry023@mymail.unisa.edu.au>
2024-10-03 21:16:03 +0930

    SCRUM-15

---
+++ WebBuilding_50080/Controllers/AccountController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebBuilding_50080.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
